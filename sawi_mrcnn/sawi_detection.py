c8c1093becb6a9deb3dc7e6b028ea6b6d6ba31d7
Idlan Q. Mohaimin <62230157+idlanqmohaimin@users.noreply.github.com>
2021-05-30 22:02:36 +0800

    Update sawi_detection.py

--- sawi_mrcnn/sawi_detection.py
+++ sawi_mrcnn/sawi_detection.py
@@ -177,7 +177,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=30,
+                epochs=10,
                 layers='heads')
 
 
