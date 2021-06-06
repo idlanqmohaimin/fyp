f4f48107a8a4db4c31950d0dea59f5dc4a176fbd
Idlan Q. Mohaimin <62230157+idlanqmohaimin@users.noreply.github.com>
2021-06-06 17:12:52 +0800

    Update sawi_detection.py

--- sawi_mrcnn/sawi_detection.py
+++ sawi_mrcnn/sawi_detection.py
@@ -40,7 +40,7 @@ class CustomConfig(Config):
     NUM_CLASSES = 1 + 1  # Background + phone,laptop and mobile
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 10
+    STEPS_PER_EPOCH = 100
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
